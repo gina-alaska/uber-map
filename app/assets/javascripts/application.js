67bb464fe7865561d3f2144860c315c3b50d297d
Will Fisher <will@teknofire.net>
2012-05-30 14:20:44 -0800

    Initial map ui implementation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require bootstrap-button
 //= require_tree .
---
+++ app/controllers/maps_controller.rb
@@ -0,0 +1,8 @@
+class MapsController < ApplicationController
+  def show
+  end
+  
+  def new
+    render :layout => false
+  end
+end
