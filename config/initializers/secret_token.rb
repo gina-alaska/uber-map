f1b0199066e65078f77742d281cabba49003f9b8
Will Fisher <will@teknofire.net>
2012-05-30 10:41:49 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ubermap::Application.config.secret_token = 'bc42ce18b8f6d3b95bb8db0ac8e35439e8556cf00742a0cb5349a6a954eca5613376def0593974e43c96547799ed6d5496e20099f61e10482627f770a5ee4317'
